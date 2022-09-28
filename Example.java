c11f999cf1202ef144f70abed64474c9fba1680c
Ragulgithub <114588808+Ragulgithub@users.noreply.github.com>
2022-09-28 18:34:57 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,21 @@
+class Example {
+void show(){
+    int a  = 10;
+    short b = 300;
+    long c = 45678;
+    float d  = 1;
+    double e =2.5678;	
+    char f = 'r';
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+}
+public static void main(String[] args){
+    Example obj = new Example();
+    obj.show();
+}
+
+}
---
+++ example.java
@@ -0,0 +1,30 @@
+class Example {
+void data(){
+     int a  = 10;
+    short b = 300;
+    long c = 45678;
+    float d  = 100.9f;
+    double e =2.5678;	
+    char f = 'r';
+    System.out.println("inerger"+a);
+    System.out.println("short"+b);
+    System.out.println("long"+c);
+    System.out.println("float"+d);      
+    System.out.println("char"+f);
+
+}
+public static void main(String[] args){
+    Example datatypes = new Example();
+    datatypes.data();
+}
+}  
+
+
+
+
+
+
+
+
+
+
